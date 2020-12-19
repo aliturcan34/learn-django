efc4bc8a463e7dc7c16e8a28358d9a5d3f4d3b02
aliturcan34 <alikaganturcan@gmail.com>
2020-12-19 12:20:20 -0500

    hide key

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r165et$v%42was1#3r5x%-tg27jnt57$+^(e!wnn3$nfw+4cqw'
-
+SECRET_KEY = os.environ.get('DJANGO')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
